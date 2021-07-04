fd61214017821e709ec973ad7f8bfdfc9d51fb83
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-07-04 14:32:21 +0900

    #typeorm connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+import { Episode } from './podcasts/entities/episode.entity';
 import { Module } from '@nestjs/common';
 import { PodcastsModule } from './podcasts/podcasts.module';
 import { GraphQLModule } from '@nestjs/graphql';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Podcast } from './podcasts/entities/podcast.entity';
 @Module({
   imports: [
+    TypeOrmModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
